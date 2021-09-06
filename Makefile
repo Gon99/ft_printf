c5e18e1809b54465c3e21bacec8059733a117e32
Gonzalo Liano ricoy <goliano-@c1r1s4.42madrid.com>
2021-09-06 16:00:02 +0200

    printf v0.1

---
+++ Makefile
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: goliano- <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/09/06 12:24:54 by goliano-          #+#    #+#              #
+#    Updated: 2021/09/06 12:29:27 by goliano-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRCS			=	./srcs/ft_printf.c
+
+OBJS			= $(SRCS:.c=.o)
+
+BONUS_OBJS		= $(BONUS:.c=.o)
+
+CC				= gcc
+RM				= rm -f
+CFLAGS			= -Wall -Wextra -Werror
+
+NAME			= libftprintf.a
+
+all:			$(NAME)
+
+$(NAME):		$(OBJS)
+				ar rcs $(NAME) $(OBJS)
+
+clean:
+				$(RM) $(OBJS) $(BONUS_OBJS)
+
+fclean:			clean
+				$(RM) $(NAME)
+
+re:				fclean $(NAME)
+
+bonus:			$(OBJS) $(BONUS_OBJS)
+				ar rcs $(NAME) $(OBJS) $(BONUS_OBJS)
+
+.PHONY:			all clean fclean re bonus
+
+
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "../libft/libft.h"
+
 #include <stdarg.h>
-#include <string.h>
 
 void print_str(char *cad)
 {
@@ -9,12 +8,46 @@ void print_str(char *cad)
 	char *word;
 
 	i = 0;
-	word = strdup(cad);
+	word = ft_strdup(cad);
 	while (word[i])
 	{
 		write(1, &word[i], 1);
 		i++;
 	}
+	free(word);
+}
+
+int	to_hex(int n)
+{
+	
+}
+
+static int handle_formats(char c1, char c2, va_list args)
+{
+	int	ret;
+
+	ret = 1;
+	if (c1 == '%' && c2 == 'c')	
+		ft_putchar_fd(va_arg(args, int), 1);
+	else if (c1 == '%' && c2 == 's')	
+		print_str(va_arg(args, char *));
+	/*else if (c1 == '%' && c2 == 'p')	
+		print_pointer(va_arg(args, char *));*/
+	else if (c1 == '%' && c2 == 'd')	
+		ft_putnbr_fd(va_arg(args, int), 1);
+	else if (c1 == '%' && c2 == 'i')	
+		ft_putnbr_fd(va_arg(args, int), 1);
+	/*else if (c1 == '%' && c2 == 'u')	
+		print_unsigned_int(va_arg(args, char *));
+	else if (c1 == '%' && c2 == 'x')	
+		print_hex_low(va_arg(args, char *));
+	else if (c1 == '%' && c2 == 'X')	
+		print_hex_up(va_arg(args, char *));
+	else if (c1 == '%' && c2 == '%')	
+		print_p_sign(va_arg(args, char *));*/
+	else
+		ret = 0;
+	return (ret);
 }
 
 int ft_printf(const char *cad, ...)
@@ -26,12 +59,11 @@ int ft_printf(const char *cad, ...)
 	va_start(args, cad);
 	while (cad[i])
 	{
-		if (cad[i] == '%' && cad[i + 1] == 's')
-		{
-			print_str(va_arg(args, char*));
-		}
-		write(1, &cad, 1);
-		cad++;
+		if (handle_formats(cad[i], cad[i + 1], args) == 1)
+			i++;
+		else
+			write(1, &cad[i], 1);
+		i++;
 	}
 	va_end(args);
 	return (0);
@@ -43,7 +75,8 @@ int main()
 	//int n = 5;
 	//float o = 5.2;
 
-	ft_printf("1: %s\n2:%s\n", "hola", "prueba");
+	ft_printf("1: %s\n2: %s\n3: %c\n4: %d\n5: %i\n6: %i\n", "hola", "prueba", 'p', 2147483647, 0, -202);
+	//printf("1: %s\n2: %s\n3: %c\n4: %d\n5: %i\n6: %i\n", "hola", "prueba", 'p', 2147483648, 0, -202);
 	//ft_printf("%s\n %d\n %f\n", cad, n, o);
 	//ft_printf("%s", cad);
 }
